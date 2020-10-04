52a7f7969c98129c2fc8b5b5206632bcf4558af6
davinson <davinson_bmx@hotmail.com>
2020-10-04 12:39:15 -0500

    Flujos para casos de multiplicacionTes

--- pruebasTaller/spec/davinson_test.spec.js
+++ pruebasTaller/spec/davinson_test.spec.js
@@ -1,1 +1,24 @@
 var principal = require('../principal/principal');
+
+describe('Mi test', ()=>{
+    it('Flujo 4', ()=>{
+        var resultado = principal.calculadora(3, 5, '*');
+        expect(resultado).toBe(15);
+    });
+
+    it('Flujo 5', ()=>{
+        var resultado = principal.calculadora(-3, 5, '*');
+        expect(resultado).toBe(-15);
+    });
+    
+    it('Flujo 6', ()=>{
+        var resultado = principal.calculadora(-3, -5, '*');
+        expect(resultado).toBe(15);
+    });
+    it('Flujo 7', ()=>{
+        var resultado = principal.calculadora(-3, 0, '*');
+        expect(resultado).toBe(0);
+    });
+});
+
+
