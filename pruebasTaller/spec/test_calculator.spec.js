ec7af197465a9eae0cb72034d87f6b5b5c797233
SaraOrozco <sara.vanesa@hotmail.com>
2020-10-18 17:33:17 -0500

    Test

--- pruebasTaller/spec/test_calculator.spec.js
+++ pruebasTaller/spec/test_calculator.spec.js
@@ -11,4 +11,44 @@ describe('Mi test', () => {
         expect(resultado).toBe(0);
     });
 
+    it('Flujo 3', () => {
+        var resultado = principal.calculadora('sin(pi/2)');
+        expect(resultado).toBe(1);
+    });
+
+    it('Flujo 4', () => {
+        var resultado = principal.calculadora('tan(pi)');
+        expect(resultado).toBe(0);
+    });
+
+    it('Flujo 5', () => {
+        var resultado = principal.calculadora('cot(1)');
+        expect(resultado).toBe(0.64);
+    });
+
+    it('Flujo 6', () => {
+        var resultado = principal.calculadora('abs(-5)');
+        expect(resultado).toBe(5);
+    });
+
+    it('Flujo 7', () => {
+        var resultado = principal.calculadora('max(-50, 0, 5)');
+        expect(resultado).toBe(5);
+    });
+
+    it('Flujo 8', () => {
+        var resultado = principal.calculadora('min(-50, 0, 5)');
+        expect(resultado).toBe(-50);
+    });
+
+    it('Flujo 9', () => {
+        var resultado = principal.calculadora('log10(1)');
+        expect(resultado).toBe(0);
+    });
+
+    it('Flujo 10', () => {
+        var resultado = principal.calculadora('round(-20.56)');
+        expect(resultado).toBe(-21);
+    });
+
 });
