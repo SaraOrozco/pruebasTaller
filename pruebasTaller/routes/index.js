fc4d7a5a660ede0296a6872a172ddf781ba568e8
daniellop31 <daniellop31@yahoo.es>
2020-10-18 19:54:06 -0500

    INTERFAZ PUG#1

--- pruebasTaller/routes/index.js
+++ pruebasTaller/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var principal = require('../principal/principal');
+var Math = require('mathjs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/operation', function(req, res, next){  
+  //const  {strOperation} = req.body;
+  console.log(req.body.strOperation);
+  console.log('Hablalo mi faiii');
+  var strResultado = '';
+  if (req.body.strOperation == ''){
+    res.render('index', { title: 'NO SE DETECTO UNA OPERACION'});   
+  } else {
+    strResultado = principal.calculadora(req.body.strOperation);
+    res.render('index', { title: 'Resultado de la operacion : '+ req.body.strOperation + ' = ' + strResultado });   
+  }  
 });
 
 module.exports = router;
